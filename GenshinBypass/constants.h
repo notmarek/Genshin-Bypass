a03cd2cb8d83dac44e20b1d75ed2149c0cc3d70f
Kento Oki <37926134+kkent030315@users.noreply.github.com>
2020-10-03 09:25:18 +0900

    code cleanup

--- GenshinBypass/constants.h
+++ GenshinBypass/constants.h
@@ -1,10 +1,7 @@
 #pragma once
 
-#define WINDOW_TITLE "å¥ê_"
+#define WINDOW_TITLE "≈í¬¥¬ê_"
 #define PROCESS_NAME "GenshinImpact.exe"
-
-//#define WINDOW_TITLE "ñ≥ëË - ÉÅÉÇí†"
-//#define PROCESS_NAME "notepad.exe"
 
 #define CLASS_NAME "UnityWndClass"
 #define MAX_WINDOW_TITLE_SIZE 256
